7f57cabcdf8f2a262c6ed6d4d81a5085426c57e1
Sara <sandra.hauzmayer@uni-ak.ac.at>
2019-03-19 20:24:24 +0100

    creating prod.env-skel file to make sure necessary config settings are available

---
+++ config/prod.env-skel.js
@@ -0,0 +1,12 @@
+'use strict'
+module.exports= {
+  NODE_ENV: '"production"',
+  API: '"http://localhost:8200/api/v1/"',
+  AUTH: '"http://localhost:8200/portfolio/accounts/"',
+  HEADER: '"bs/js/base-header.128dbd69b0fb3491cc0d.js"',
+  APP_PREFIX: '"/portfolio"',
+  PORTFOLIO_API: '"http://localhost:8200"',
+  SKOSMOS_API: '"https://voc.uni-ak.ac.at/skosmos/rest/v1/"',
+  LOCALES: ['de', 'en'],
+  DEFAULT_LOCALE: 'de',
+}
