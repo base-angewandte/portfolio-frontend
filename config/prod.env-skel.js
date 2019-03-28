687001ddaf8b999df98b05d84f16bf8e6d1d88dc
Sandra Hauzmayer <sandra.hauzmayer@platomics.com>
2019-03-28 10:36:59 +0100

    fix: modifying settings to prevent webpack-dev-server failing

--- config/prod.env-skel.js
+++ config/prod.env-skel.js
@@ -1,9 +1,9 @@
 'use strict'
 module.exports= {
   NODE_ENV: '"production"',
-  HOST: '"fe.basedev.uni-ak.ac.at"',
-  PORT: '8080',
-  HTTPS: 'false',
+  HOST: 'fe.basedev.uni-ak.ac.at',
+  PORT: 8080,
+  HTTPS: false,
   API: '"https://basedev.uni-ak.ac.at/portfolio/api/v1/"',
   HEADER: '"bs/js/base-header.128dbd69b0fb3491cc0d.js"',
   APP_PREFIX: '"/portfolio"',
