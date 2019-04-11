584a16b756b2305b780dca9f5ddd4b333eb24689
Sandra Hauzmayer <sandra.hauzmayer@platomics.com>
2019-04-11 11:06:28 +0200

    #659: fixing files area not updating on upload and adding status indicator

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -5,8 +5,8 @@
     title="Dateien hochladen"
     button-left-text="Abbrechen"
     @button-right="startUpload"
-    @button-left="$emit('cancel')"
-    @close="$emit('cancel')">
+    @button-left="cancelUpload('cancel')"
+    @close="cancelUpload('done')">
     <div
       class="popup-upload-area">
       <BaseUploadBar
@@ -14,35 +14,30 @@
         :key="index"
         :progress="uploadPercentage[index]"
         :filename="file.name"
+        :status="getStatus(file.name)"
         class="upload-bar"/>
     </div>
     <div class="popup-text">
       <BaseDropDown
         :label="$t('form-view.chooseLicense')"
-        :selected="{
-          label: $t('nolicense'),
-          value: '',
-        }"
-        :selection-list="[{
-                            label: 'CC-BY',
-                            value: 'cc-by',
-                          },
-                          {
-                            label: 'CC0',
-                            value: 'cc0',
-                          }
+        :selection-list="[
+          {
+            label: 'CC0',
+            value: 'cc0',
+          }
         ]"
         :background-color="'rgb(240, 240, 240)'"
         :fixed-width="true"
+        v-model="license"
         header-style="inline"/>
       <BaseDropDown
-        :selected="{ label: $t('no'), value: false }"
         :selection-list="[
-          { label: $t('no'), value: false },
-          { label: $t('yes'), value: true }]"
+          { label: $t('no'), value: 'false' },
+          { label: $t('yes'), value: 'true' }]"
         :background-color="'rgb(240, 240, 240)'"
         :fixed-width="true"
         :label="$t('form-view.showImages')"
+        v-model="publish"
         header-style="inline"/>
     </div>
   </BasePopUp>
@@ -81,10 +76,15 @@ export default {
   data() {
     return {
       uploadedFiles: [],
+      rejectedFiles: [],
       uploadError: null,
       currentStatus: null,
-      uploadFileName: 'photos',
       uploadPercentage: [],
+      publish: { label: this.$t('no'), value: 'false' },
+      license: {
+        label: 'CC-BY',
+        value: 'cc-by',
+      },
     };
   },
   computed: {
@@ -110,46 +110,73 @@ export default {
         this.$emit('success');
       } else {
         if (!this.fileList.length) return;
-        this.fileList.forEach(async (file, index) => {
-          const formData = new FormData();
-          formData.append('file', file);
-          formData.append('entry', this.$route.params.id);
-          formData.append('published', false);
+        await Promise.all(this.fileList
+          .map((file, index) => new Promise(async (resolve, reject) => {
+            const formData = new FormData();
 
-          this.currentStatus = STATUS_SAVING;
-          this.$emit('upload-start');
+            formData.append('file', file);
+            formData.append('entry', this.$route.params.id);
+            // These values can not be empty (also no empty string) or it will give
+            // an error
+            formData.append('published', this.publish.value);
+            formData.append('license', this.license.value);
 
-          try {
-            const res = await axios.post(`${process.env.API}media/`,
-              formData,
-              {
-                withCredentials: true,
-                xsrfCookieName: 'csrftoken_portfolio',
-                xsrfHeaderName: 'X-CSRFToken',
-                headers: {
-                  'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW',
-                },
-                onUploadProgress: (progressEvent) => {
-                  this.uploadPercentage[index] = Math
-                    .round(progressEvent.loaded / progressEvent.total);
-                },
-              });
-            this.uploadedFiles = [].concat(res);
-            this.currentStatus = STATUS_SUCCESS;
-          } catch (err) {
-            this.uploadError = err.response;
-            this.currentStatus = STATUS_FAILED;
-            console.log(err);
-          }
-        });
-        this.$store.dispatch('data/fetchMediaData', this.$route.params.id);
+            this.currentStatus = STATUS_SAVING;
+            this.$emit('upload-start');
+
+            try {
+              await axios.post(`${process.env.API}medipa/`,
+                formData,
+                {
+                  withCredentials: true,
+                  xsrfCookieName: 'csrftoken_portfolio',
+                  xsrfHeaderName: 'X-CSRFToken',
+                  headers: {
+                    'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW',
+                  },
+                  onUploadProgress: (progressEvent) => {
+                    this.uploadPercentage[index] = Math
+                      .round(progressEvent.loaded / progressEvent.total);
+                  },
+                });
+              this.uploadedFiles.push(file.name);
+              this.currentStatus = STATUS_SUCCESS;
+              resolve();
+            } catch (err) {
+              this.uploadPercentage[index] = 0;
+              this.uploadError = err.response;
+              this.currentStatus = STATUS_FAILED;
+              this.rejectedFiles.push(file.name);
+              reject();
+            }
+          })));
+        await this.$store.dispatch('data/fetchMediaData', this.$route.params.id);
       }
+    },
+    cancelUpload() {
+      this.$emit('cancel');
+      this.reset();
     },
     reset() {
       this.currentStatus = STATUS_INITIAL;
       this.uploadedFiles = [];
+      this.rejectedFiles = [];
+      this.publish = { label: this.$t('no'), value: false };
+      this.license = {
+        label: 'CC-BY',
+        value: 'cc-by',
+      };
       this.uploadError = null;
       this.fileList.forEach((file, index) => this.$set(this.uploadPercentage, index, 0));
+    },
+    getStatus(fileName) {
+      if (this.uploadedFiles.includes(fileName)) {
+        return 'success';
+      }
+      if (this.rejectedFiles.includes(fileName)) {
+        return 'fail';
+      }
+      return '';
     },
   },
 };
