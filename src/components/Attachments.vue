cf3d0866dfd0594e593a89ef8aa69f393d435549
shauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2019-05-13 11:21:19 +0200

    fix: new build with corrrected autosuggest path

--- src/components/Attachments.vue
+++ src/components/Attachments.vue
@@ -425,8 +425,6 @@ export default {
         } if (iconName.includes('http')) {
           return iconName;
         }
-        console.log(process.env.PORTFOLIO_HOST);
-        console.log(`${process.env.PORTFOLIO_HOST}${iconName}`);
         return `${process.env.PORTFOLIO_HOST}${iconName}`;
       }
       return '';
