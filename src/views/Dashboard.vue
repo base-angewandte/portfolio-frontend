c12de3620d6381ca3545c2766543fa6a5d78aa63
shauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2019-06-17 13:11:39 +0200

    feat: adding download functionality

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -22,7 +22,8 @@
       :show-preview="showPreview"
       :media-url="previewUrl"
       :display-size="previewSize"
-      @hide-preview="showPreview = false"/>
+      @hide-preview="showPreview = false"
+      @download="downloadFile"/>
 
     <Sidebar
       ref="sidebar"
@@ -43,6 +44,7 @@
 
 <script>
 import { BasePopUp, BaseMediaPreview } from 'base-ui-components';
+import axios from 'axios';
 import Sidebar from '../components/Sidebar';
 import { capitalizeString } from '../utils/commonUtils';
 
@@ -159,6 +161,29 @@ export default {
         return capitalizeString(text);
       }
       return '';
+    },
+    async downloadFile({ url, name }) {
+      try {
+        const { data } = await axios({
+          method: 'get',
+          url,
+          responseType: 'blob',
+        });
+        const fileUrl = window.URL.createObjectURL(new Blob([data]));
+        const link = document.createElement('a');
+        link.href = fileUrl;
+        link.setAttribute('download', name);
+        document.body.appendChild(link);
+        link.click();
+      } catch (e) {
+        this.showPreview = false;
+        this.$notify({
+          group: 'request-notifications',
+          title: this.$t('notify.somethingWrong'),
+          text: `An error occured during the download of file ${name}`,
+          type: 'error',
+        });
+      }
     },
   },
 };
