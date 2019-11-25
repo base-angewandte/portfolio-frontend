0edb29fe378d3f5dd1f996b66af52ac7a9912463
shauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2019-11-25 12:24:19 +0100

    fix: #931: field groups unsaved changes triggered and correct handling of clear all fields

--- src/components/BaseForm.vue
+++ src/components/BaseForm.vue
@@ -362,11 +362,15 @@ export default {
       this.multiplyParams = { index: this.valueListInt[field.name].length - 1, name: field.name };
     },
     removeField(field, index) {
-      if (index) {
-        this.valueListInt[field.name].splice(index, 1);
+      const fieldGroupValues = this.valueListInt[field.name];
+      // only splice off group if more than one field visible
+      if (fieldGroupValues && fieldGroupValues.length > 1) {
+        fieldGroupValues.splice(index, 1);
         this.$emit('values-changed', this.valueListInt);
+        // else just clear the fields
       } else {
-        this.$set(this.valueList, field.name, this.getInitialFieldValue(field.items));
+        this.$set(fieldGroupValues, index, this.getInitialFieldValue(field.items));
+        this.$emit('values-changed', this.valueListInt);
       }
     },
     isHalfField(field) {
