a2f1c7dde524cfe3969d16e81d7eed47164e336c
Sandra Hauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2021-05-10 15:29:16 +0200

    fix: #1360: update dependencies and fix security vulnerabilites

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config({
-  path: './.env.production.local',
+  path: './.env.local',
 });
 
 const app = express();
