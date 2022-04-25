d8dec2008b7e3ee04413eda4a9b68b19247cb191
bsaltuari <bernhard.saltuari@uni-ak.ac.at>
2022-04-25 15:38:51 +0200

    feat: #1911: try several times get and set the async showroom_id after publishing the entry

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -243,6 +243,8 @@ export default {
        * idChanged is true)
        */
       idChanged: false,
+      // variable to count requests to get showroom_id after publishing entry
+      counter: 0,
     };
   },
   computed: {
@@ -503,6 +505,22 @@ export default {
     // isFormSaved with the opposite Boolean value
     unsavedChanges() {
       this.$store.commit('data/setIsFormSaved', !this.unsavedChanges);
+    },
+    // watch for published state and showroom_id
+    // fetch or reset showroom_id
+    valueList: {
+      handler(val) {
+        if (!val.showroom_id && val.published) {
+          this.counter = 0;
+          this.fetchShowroomId();
+        }
+
+        if (!val.published) {
+          this.valueList.showroom_id = null;
+        }
+      },
+      immediate: true,
+      deep: true,
     },
   },
   async beforeCreate() {
@@ -1400,6 +1418,27 @@ export default {
       this.formIsLoading = true;
       this.valueList = JSON.parse(JSON.stringify(this.valueListOriginal));
       this.formIsLoading = false;
+    },
+    /**
+     * Try several times to get the entry data and set the showroom_id
+     *
+     * @param {number} attempts - amount of attempts
+     * @param {number} delay - delay in ms
+     */
+    async fetchShowroomId(attempts = 3, delay = 10000) {
+      try {
+        const entry = await this.$store.dispatch('PortfolioAPI/get', { kind: 'entry', id: this.currentItemId });
+        this.valueList.showroom_id = entry.showroom_id;
+      } catch (e) {
+        console.warn(`showroom_id could not be fetched after ${this.counter} requests.`);
+      }
+
+      if (!this.valueList.showroom_id && this.counter < attempts) {
+        setTimeout(() => {
+          this.fetchShowroomId();
+        }, delay);
+        this.counter += 1;
+      }
     },
   },
 };
