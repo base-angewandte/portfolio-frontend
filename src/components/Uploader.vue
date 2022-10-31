70c78eb1d060e9184566f68f5b98db24d501899a
bsaltuari <bernhard.saltuari@uni-ak.ac.at>
2022-10-31 13:22:56 +0100

    feat: #2204: file uploads: handle header 400 errors

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -13,6 +13,7 @@
     :current-status="currentStatus"
     :file-list="fileList"
     :rejected-files="rejectedFiles"
+    :file-errors="fileErrors"
     :user-space="userSpace"
     :upload-percentage="uploadPercentage"
     :uploaded-files="uploadedFiles"
@@ -74,6 +75,7 @@ export default {
       publish: { label: this.$t('no'), value: 'false' },
       license: {},
       disabled: true,
+      fileErrors: [],
     };
   },
   computed: {
@@ -135,7 +137,7 @@ export default {
           await Promise.all(this.fileList
             // eslint-disable-next-line no-async-promise-executor
             .map((file, index) => new Promise(async (resolve, reject) => {
-              // accounting for retrys by only acting on files that have no upload
+              // accounting for retries by only acting on files that have no upload
               // percentage yet
               if (this.uploadPercentage[index] === 0) {
                 const formData = new FormData();
@@ -175,6 +177,14 @@ export default {
                   this.uploadError = err.response;
                   this.currentStatus = STATUS_FAILED;
                   this.rejectedFiles.push(file.name);
+                  // add error messages by file name
+                  this.fileErrors.push({
+                    name: file.name,
+                    message: Array.isArray(err.response.data.file)
+                      ? err.response.data.file.join(', ')
+                      : err.response.data.file,
+                  });
+
                   reject(err);
                 }
               }
@@ -219,6 +229,7 @@ export default {
       this.currentStatus = STATUS_INITIAL;
       this.uploadedFiles = [];
       this.rejectedFiles = [];
+      this.fileErrors = [];
       this.publish = { label: this.$t('no'), value: false };
       this.license = {
         label: this.$t('nolicense'),
@@ -226,15 +237,6 @@ export default {
       };
       this.uploadError = null;
       this.fileList.forEach((file, index) => this.$set(this.uploadPercentage, index, 0));
-    },
-    getStatus(fileName) {
-      if (this.uploadedFiles.includes(fileName)) {
-        return 'success';
-      }
-      if (this.rejectedFiles.includes(fileName)) {
-        return 'fail';
-      }
-      return '';
     },
     removeFile(index) {
       this.fileList.splice(index, 1);
