dbe19de42e6e2ee398fbcddeacce14e234501b67
Sandra Hauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2022-11-29 13:59:58 +0100

    build: add conventional changelog and gulp script for automated changelog

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const fs = require('fs');
-const gulp = require('gulp')
-const axios = require('axios')
-const log = require('fancy-log')
-const replace = require('gulp-replace')
+const gulp = require('gulp');
+const axios = require('axios');
+const log = require('fancy-log');
+const replace = require('gulp-replace');
+const conventionalChangelog = require('gulp-conventional-changelog');
 
 let envFile = './.env.local';
 
@@ -61,6 +62,102 @@ gulp.task('set-default-lists', async function () {
      Please make sure the VUE_APP_DEFAULT_LISTS variable in .env.local has an appropriate value!`)
     log.error(e);
   }
+});
+
+gulp.task('changelog', (cb) => {
+  // check if the file exists and create it if necessary
+  // fallback if user forgets to create an .env.local file
+  if (!fs.existsSync('CHANGELOG.md')) {
+    fs.openSync('CHANGELOG.md', 'w');
+    console.info('CHANGELOG.md file was created.');
+  }
+
+  gulp.src('CHANGELOG.md')
+    .pipe(conventionalChangelog({
+      // conventional-changelog options go here
+      preset: 'conventionalcommits',
+      append: false,
+      releaseCount: 1,
+    }, {
+      // do not link references to github repo
+      linkReferences: false,
+    }, {
+      // git-raw-commits options go here
+    }, {
+      // conventional-commits-parser options go here
+    }, {
+      // sections are created based on commit type
+      groupBy: 'type',
+      reverse: false,
+      // order by scope first so that components affecting same component are at least
+      // listed below each other
+      commitsSort: ['scope', 'subject'],
+      // sort the sections according to a predefined order
+      commitGroupsSort: (val1, val2) => {
+        // define groups for which order is fixed here
+        // (everything else will come after)
+        const sortOrder = ['feat', 'change', 'deprecate', 'remove', 'fix', 'security'];
+        const val1Index = sortOrder.indexOf(val1.title);
+        const val2Index = sortOrder.indexOf(val2.title);
+        // if val1 does not exist in the order array sort val2 before val1
+        if (val1Index === -1) {
+          return 1;
+        }
+        // if val2 does not exist in array sort val1 before val1
+        if (val2Index === -1) {
+          return -1;
+        }
+        // else sort according to the index value in sort array
+        return val1Index - val2Index;
+      },
+      // do transformations for every single commit here
+      // (also tried with transform function of conventional-changelog options however
+      // outcome was quite different so leaving it here)
+      // needing to keep this otherwise style will be the original one (Bug fixes etc)??
+      transform: (commit) => {
+        // initialize an object for the modified properties (so there is potential
+        // to modify further props)
+        const modifiedProps = {};
+        // return original commit object together with modified props overwriting original
+        // if necessary via spread operator
+        return {
+          ...commit,
+          ...modifiedProps,
+        };
+      },
+      finalizeContext: (context) => {
+        // define which type should go into which section here (as specified in documentation)
+        const changelogGroups = {
+          feat: 'Added',
+          change: 'Changed',
+          deprecate: 'Deprecated',
+          remove: 'Removed',
+          fix: 'Fixed',
+          security: 'Security',
+        };
+        return {
+          ...context,
+          // create custom section headers instead of the default type derived ones
+          commitGroups: context.commitGroups.map((group) => {
+            // in order to exclude all groups that are not defined in the documentation
+            // (https://hedgedoc.uni-ak.ac.at/#Changelog)
+            // if undefined groups should be left in there just leave first condition!
+            if (group.title) {
+              const newTitleString = changelogGroups[group.title] || group.title;
+              return {
+                ...group,
+                // check if there is actually a title string and if yes uppercase it
+                title: newTitleString ? newTitleString.charAt(0).toUpperCase() + newTitleString.slice(1) : '',
+              };
+            }
+            // returning an empty object will ignore the section in the changelog output
+            return {};
+          }),
+        };
+      },
+    }))
+    .pipe(gulp.dest('./'));
+  cb();
 });
 
 gulp.task('default', gulp.series(gulp.parallel('set-header', 'set-default-lists')));
